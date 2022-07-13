847c8c87cbe4f652521654244195427c847e0eff
Daniel Farias <danielfariias15@gmail.com>
2022-07-13 20:04:23 -0300

    config eslit & editorconfig

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
-app.listen(3000, () => console.log('🔥 Server started at port 3000'))
+app.listen(3000, () => console.log('🔥 Server started at port 3000'));
