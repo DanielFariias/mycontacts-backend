ab592462bb4125f5f7147761b68e5c563c10ce96
Daniel Farias <danielfariias15@gmail.com>
2022-07-13 22:50:50 -0300

    create delete & show contactController

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,12 +1,12 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
-const contacts = [
+let contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: 'Daniel',
     email: 'daniel@daniel.com',
     phone: '99 99999-9999',
-    category_id: uuid(),
+    category_id: v4(),
   },
 ];
 
@@ -14,6 +14,21 @@ class ContactsRepository {
   findAll() {
     return new Promise((resolve) => {
       resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => {
+      resolve(
+        contacts.find((contact) => contact.id === id),
+      );
+    });
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
     });
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,7 @@ const ContactController = require('./app/controllers/ContactContoller');
 const routes = Router();
 
 routes.get('/contacs', ContactController.index);
+routes.get('/contacs/:id', ContactController.show);
+routes.delete('/contacs/:id', ContactController.delete);
 
 module.exports = routes;
